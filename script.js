9f502c351d4d5bfc153e43efd2968cdc4df8fa86
Zack Hartmann <zackhartmann@gmail.com>
2017-07-25 18:19:45 -0400

    Corrected

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
 
-	var queryTextInput = document.getElementById('queryText').value;
+	var queryButton = document.getElementById('queryButton');
 
-	console.log(queryTextInput);
-  // Using jQuery useragent on WikiAPI
-  $.ajax( {
-    url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + queryTextInput,
-    // data: queryData,
-    dataType: 'jsonp',
-    type: 'POST',
-    headers: { 'Api-User-Agent': 'Example/1.0' },
-    success: function(data) {
-    	console.log(data);
-    }
-  });
-})
+	queryButton.addEventListener('click', function() {
+		var queryTextInput = document.getElementById('queryText').value;
+		$.ajax( { // Using jQuery useragent on WikiAPI
+			url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + queryTextInput,
+			// data: queryData,
+			dataType: 'jsonp',
+			type: 'POST',
+			// type: 'GET',
+			headers: { 'Api-User-Agent': 'Example/1.0' },
+			success: function(responseData) {
+				// var pages = responseData.query.pages;
+				console.log(responseData);
+				// $.each(pages, function(index, source){
+				// console.log(pages[index]);
+				// var sources = response.sources;
+				// var html =  '<select class="form-control" id="source">';
+				// var pagesUL = document.querySelector('ul');
+				// pagesUL.innerHTML = '';
+				// $.each(sources, function(index, responseData){
+				// 	pagesUL.appendChild(responseData.sources);
+			}
+		});  // End of ajax
+	}) // End of Event Listener
+});
